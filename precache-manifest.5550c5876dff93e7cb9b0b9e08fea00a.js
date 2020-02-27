d4de068e808f702f2d5374011d4531ea4ec0b3c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 12:12:11 +0000

    Deploy taeukme/covid-19-jeju-hospital-map to github.com/taeukme/covid-19-jeju-hospital-map.git:gh-pages

---
+++ precache-manifest.5550c5876dff93e7cb9b0b9e08fea00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4eaacfd1fd21c3b6bf55dba48d5c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691771cf6721e0530b2c",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "93fb38f7dd57eb04c625",
+    "url": "/static/js/2.d1261851.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.d1261851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691771cf6721e0530b2c",
+    "url": "/static/js/main.734bcb85.chunk.js"
+  },
+  {
+    "revision": "8e65dc9c065515220c5a",
+    "url": "/static/js/runtime-main.a99765c6.js"
+  }
+]);
